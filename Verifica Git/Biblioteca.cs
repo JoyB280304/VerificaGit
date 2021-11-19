099cfee1613ff3543a37eb71cc23fd4099f0c96a
JoyB280304 <joy.beshiri.stud@ispascalcomandini.it>
2021-11-19 09:20:36 +0100

    V1: CLASSE CREATA

--- Verifica Git/Biblioteca.cs
+++ Verifica Git/Biblioteca.cs
@@ -8,5 +8,47 @@ namespace Verifica_Git
 {
     class Biblioteca
     {
+        private string _nome;
+        private string _indirizzo;
+        private string _apertura;
+        private string _chiusura;
+        private List<Libro> _listaLibri;
+
+        public Biblioteca(string nome, string indirizzo, string apertura, string chiusura, List<Libro> libri)
+        {
+            Nome = nome;
+            Indirizzo = indirizzo;
+            Apertura = apertura;
+            Chiusura = chiusura;
+            _listaLibri = libri;
+        }
+
+        public string Nome
+        {
+            set { _nome = value; }
+
+            get { return _nome; }
+        }
+
+        public string Indirizzo
+        {
+            set { _indirizzo = value; }
+
+            get { return _indirizzo; }
+        }
+
+        public string Apertura
+        {
+            set { _apertura = value; }
+
+            get { return _apertura; }
+        }
+
+        public string Chiusura
+        {
+            set { _chiusura = value; }
+
+            get { return _chiusura; }
+        }
     }
 }
